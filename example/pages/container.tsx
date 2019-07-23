d56aa5dc8e16f2250f4de117034028563e8a5d6e
ChenYong <chenyong@jimeng.io>
2019-07-23 10:50:36 +0800

    use a richer sidebar

--- example/pages/container.tsx
+++ example/pages/container.tsx
@@ -20,73 +20,129 @@ import FormBlankLabel from "forms/blank-label";
 import DrawerPage from "forms/drawer";
 import CustomMultiplePage from "forms/custom-multiple";
 import GroupPage from "forms/group";
+import { DocSidebar, ISidebarEntry } from "@jimengio/doc-frame";
 
-let pages: { title: string; path: string }[] = [
+let items: ISidebarEntry[] = [
   {
     title: "Basic",
+    cnTitle: "基础",
     path: genRouter.home.name,
   },
   {
     title: "Modal",
+    cnTitle: "弹出层",
     path: genRouter.modal.name,
   },
   {
     title: "Select",
+    cnTitle: "下拉选择",
     path: genRouter.select.name,
   },
   {
     title: "Draft",
+    cnTitle: "混合",
     path: genRouter.draft.name,
   },
   {
     title: "Drawer",
+    cnTitle: "抽屉",
     path: genRouter.drawer.name,
   },
   {
     title: "Validation",
+    cnTitle: "校验",
     path: genRouter.validation.name,
   },
   {
     title: "Custom",
+    cnTitle: "自定义",
     path: genRouter.custom.name,
   },
   {
     title: "Custom multiple",
+    cnTitle: "多项自定义",
     path: genRouter.customMultiple.name,
   },
   {
     title: "Auto save",
+    cnTitle: "自动保存",
     path: genRouter.autoSave.name,
   },
   {
     title: "Use meson core",
+    cnTitle: "使用 Meson code",
     path: genRouter.wrapMesonCore.name,
   },
   {
     title: "Forwarded form",
+    cnTitle: "引用传递写法",
     path: genRouter.forwardForm.name,
   },
   {
     title: "Modify on change",
+    cnTitle: "修改的钩子",
     path: genRouter.modifyOnChange.name,
   },
   {
     title: "Switch",
+    cnTitle: "开关",
     path: genRouter.switch.name,
   },
   {
     title: "Inline form",
+    cnTitle: "行内模式",
     path: genRouter.inlineForm.name,
   },
   {
     title: "Blank label",
+    cnTitle: "空白标签",
     path: genRouter.blankLabel.name,
   },
   {
     title: "Group",
+    cnTitle: "分组",
     path: genRouter.group.name,
   },
 ];
+
+let onSwitchPage = (path: string) => {
+  switch (path) {
+    case genRouter.home.name:
+      return genRouter.home.go();
+    case genRouter.draft.name:
+      return genRouter.draft.go();
+    case genRouter.modal.name:
+      return genRouter.modal.go();
+    case genRouter.drawer.name:
+      return genRouter.drawer.go();
+    case genRouter.select.name:
+      return genRouter.select.go();
+    case genRouter.validation.name:
+      return genRouter.validation.go();
+    case genRouter.custom.name:
+      return genRouter.custom.go();
+    case genRouter.customMultiple.name:
+      return genRouter.customMultiple.go();
+    case genRouter.autoSave.name:
+      return genRouter.autoSave.go();
+    case genRouter.wrapMesonCore.name:
+      return genRouter.wrapMesonCore.go();
+    case genRouter.forwardForm.name:
+      return genRouter.forwardForm.go();
+    case genRouter.modifyOnChange.name:
+      return genRouter.modifyOnChange.go();
+    case genRouter.switch.name:
+      return genRouter.switch.go();
+    case genRouter.inlineForm.name:
+      return genRouter.inlineForm.go();
+    case genRouter.blankLabel.name:
+      return genRouter.blankLabel.go();
+    case genRouter.group.name:
+      return genRouter.group.go();
+    default:
+      console.error("Unknown page", path);
+  }
+};
 
 let Container: FC<{ router: IRouteParseResult }> = (props) => {
   let renderChild = (x) => {
@@ -130,13 +186,14 @@ let Container: FC<{ router: IRouteParseResult }> = (props) => {
 
   return (
     <div className={cx(fullscreen, row, styleContainer)}>
-      <div className={cx(column, styleSidebar)}>
-        {pages.map((page) => {
-          let isActive = page.path === props.router.name;
+      <DocSidebar
+        currentPath={props.router.name}
+        onSwitch={(item) => {
+          onSwitchPage(item.path);
+        }}
+        items={items}
+      />
 
-          return <HashLink key={page.path} to={page.path} text={page.title} className={cx(styleEntry, isActive ? styleActiveEntry : null)} />;
-        })}
-      </div>
       <div>{renderChild(props.router)}</div>
     </div>
   );
