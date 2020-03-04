81dba3d2a43f339526c77c30d35be3915b7ebe46
ChenYong <chenyong@jimeng.io>
2020-03-04 18:03:55 +0800

    ensure required rule used even validator exists; alpha release

--- src/lingual/zh-cn.ts
+++ src/lingual/zh-cn.ts
@@ -2,7 +2,7 @@ import { ILang } from "./interface";
 export const zhCN: ILang = {
   cancel: "取消",
   confirm: "确认",
-  labelIsRequired: "{label} 必填",
+  labelIsRequired: "{label} 不可以为空",
   labelShouldBeBoolean: "{label} 应该为布尔值",
   labelShouldBeNumber: "{label} 应该为数字",
   labelShouldBeString: "{label} 应该为字符串",
