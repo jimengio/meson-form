9c6b59c4b16c8b1001a68782d98c6b503211997f
rebirthO <benyue.liu@jimeng.io>
2021-01-18 16:52:52 +0800

    fix: 表单提交异步验证失效问题

--- src/util/render.ts
+++ src/util/render.ts
@@ -36,3 +36,26 @@ export function traverseItemsReachCustomMultiple<T extends FieldValues>(xs: IMes
     }
   });
 }
+
+export async function asyncCustomeValidateItems<T extends FieldValues>(xs: IMesonFieldItem<T>[], form: T, method: (x: IMesonFieldItem<T>) => void) {
+  const xsPromises = xs.map(async (x) => {
+    if (x.shouldHide != null && x.shouldHide(form)) {
+      return null;
+    }
+    if (x.onlyShow != null && !x.onlyShow(form)) {
+      return null;
+    }
+
+    switch (x.type) {
+      case "decorative":
+        return;
+      case "nested":
+      case "group":
+        await asyncCustomeValidateItems(x.children, form, method);
+      default:
+        await method(x);
+    }
+  });
+
+  await Promise.all(xsPromises);
+}
